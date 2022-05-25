d1181d99a021743d094d15998da2224a91fddae4
Igor <igor.zubenko@1win.pro>
2022-05-25 22:50:29 +0300

    update log framework

--- template-literal/4-own-test-framework.js
+++ template-literal/4-own-test-framework.js
@@ -1,49 +1,80 @@
-// example
+const CONTROLS = {
+  compare: {
+    action: (a, b) => {
+      if (!(a && b)) return 'command "COMPARE" need two and more arguments'
+
+      return a === b ? 'is equal' : 'is different'
+    }
+  },
+  unite: {
+    action: (a, b) => {
+      if (typeof a  !== typeof b ) return a
+
+      const operandType = typeof a
+
+      const OPERATION_BY_TYPE = {
+        number: (a, b) => (a+b),
+        string: (a, b) => (a+b),
+        boolean: (a, b) => (a || b),
+        object: (a, b) => Array.isArray(a) ? ([...a, ...b]) : ({...a, ...b}),
+      }
+
+      const operations = Object.keys(OPERATION_BY_TYPE)
+
+      return operations.includes(operandType) ? OPERATION_BY_TYPE[operandType](a, b) : a //need calback with error
+    }
+  },
+  keep: {
+
+  },
+  get: {
+
+  },
+}
 
 /* MAIN FUNCTION */
 const createPrint = () => {
   const cache = {}
   let targetEl
-  let proccessingEl
 
   return (commands, ...templateData) => {
-    let currentAction
-    const controlMap = {
-      compare: {
-        action: (a, b) => a === b ? 'is equal' : 'is different'
-      },
-      unite: {
-        action: (a, b) => {
-          if (typeof a  !== typeof b ) return a
+    let actionOfCommand = () => {}
+    let resultOfCommand = ''
 
-          const operandType = typeof a
-
-          const OPERATION_BY_TYPE = {
-            number: (a, b) => (a+b),
-            string: (a, b) => (a+b),
-            boolean: (a, b) => (a || b),
-            object: (a, b) => Array.isArray(a) ? ([...a, ...b]) : ({...a, ...b}),
-          }
-
-          const operations = Object.keys(OPERATION_BY_TYPE)
-
-          return operations.includes(operandType) ? OPERATION_BY_TYPE[operandType](a, b) : a //need calback with error
-        }
-      },
-      put: {
-
-      },  
-      keep: {
-
-      },
-      get: {
-
-      },
-    }
-
-    const generalCommands = Object.keys(controlMap)
+    const generalCommands = Object.keys(CONTROLS)
     const uniteSymbol = '&'
     const AVAILABLE_COMMANDS = [...generalCommands, uniteSymbol] // rewrite
+
+    const commandSetReduceCallback = (templateData) => (acc, command, idx, arr) => {
+      const param = templateData[idx]
+      let args = []
+      console.log('START');
+      console.log('---------------', param, command, command in CONTROLS)
+    
+      if (command in CONTROLS) {
+        const { action } = CONTROLS[command]
+    
+        actionOfCommand = action
+        targetEl = param
+        args = [targetEl]
+        acc += resultOfCommand
+        resultOfCommand = ''
+      } else if (command === uniteSymbol) {
+        args = [targetEl, param]
+      } else {
+        return acc
+      }
+
+      console.log('action func result');
+      console.log(actionOfCommand(...args), ...args)
+      resultOfCommand = actionOfCommand(...args)
+      
+      if (idx === arr.length - 1) {
+        acc += resultOfCommand
+      }
+    
+      return  acc 
+    }
 
     const makeCommandSet = (commands) => commands
       .map(
@@ -56,28 +87,20 @@ const createPrint = () => {
     console.log("🚀 ~ file: 4-own-test-framework.js ~ line 47 ~ return ~ commandSet", commandSet)
 
     // create result string
-    const res = commandSet.reduce((acc, command, idx) => {
-      const param = templateData[idx]
-      
-      if (command in controlMap) {
-        const { action } = controlMap[command]
+    const res = commandSet.reduce(commandSetReduceCallback(templateData), '')
 
-        currentAction = action
-        acc += targetEl
-        targetEl = null
-      } else if (command === uniteSymbol) {
-        targetEl = currentAction(targetEl, param)
-      }
-      
-      return acc
-    }, '')
+    console.log( "\u001b[1;32m"+res+'\u001b[0m' );
 
-    console.log(res)
+    // cArg = [arg]
+    // cFn = func
+    // tempRes = cFn(...cArg) 
+
+    // -> func || end => acc += tempRes  and repeat
+    // -> & cArgs = [cArg, arg] cFn = cFn(cArg, arg)
 
     /*
       [compare] => 1 isEqual 2 ? is equal : is different. 1 in chain
       [unite] => if same type ? unite : 1 and error. result or 1
-      [put] => if same type ? unite : 1 and error. result or 1
       [keep] => keep in cache
       [get] => get from cache
     */
@@ -96,3 +119,5 @@ const print = createPrint()
 
 //print`compare ${fObject}&${sObject} unite ${trdObject} keep ${'memo'}`
 print`compare errorWord ${fObject}&${sObject}`
+
+print`compare ${fObject}`
