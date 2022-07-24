e345b06ff5024e018a558b030d4288745f9fda37
Igor <igor.zubenko@1win.pro>
2022-07-24 21:44:34 +0300

    create regexp engine

---
+++ regexp-engine/engine.js
@@ -0,0 +1,21 @@
+function match(regex, inputString) {
+  let result = ['']
+
+  const createInterval = (intervals = []) => {
+    
+  }
+
+  if (regex.length === 1 && createInterval([97, 122], [65, 90]).includes(regex.charCodeAt(0)))
+
+  return result
+}
+
+const serviceChar = ['$', '?', '*', '.', '^']
+
+let idx = 0
+function test(result, rightResult) {
+  console.log(`TEST #${idx++}`)
+  return result === rightResult ? console.log('CORRECT') : `Error on #${idx++}`
+}
+
+test(match(q, q), ['q'])
